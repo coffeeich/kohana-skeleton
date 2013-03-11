753d002451697cec59814b0a3c32aae9d8d85bfd
Roman Kuzmin <roman.i.kuzmin@gmail.com>
2013-03-11 09:09:03 +0400

    Add submodules init script

---
+++ add-git-submodules.php
@@ -0,0 +1,64 @@
+<?php
+
+$submodule = array(
+    array(
+        'path' => 'system',
+        'url' => 'https://github.com/kohana/core.git',
+    ),
+    array(
+        'path' => 'modules/auth',
+        'url' => 'https://github.com/kohana/auth.git',
+    ),
+    array(
+        'path' => 'modules/cache',
+        'url' => 'https://github.com/kohana/cache.git',
+    ),
+    array(
+        'path' => 'modules/codebench',
+        'url' => 'https://github.com/kohana/codebench.git',
+    ),
+    array(
+        'path' => 'modules/database',
+        'url' => 'https://github.com/kohana/database.git',
+    ),
+    array(
+        'path' => 'modules/image',
+        'url' => 'https://github.com/kohana/image.git',
+    ),
+    array(
+        'path' => 'modules/minion',
+        'url' => 'https://github.com/kohana/minion.git',
+    ),
+    array(
+        'path' => 'modules/orm',
+        'url' => 'https://github.com/kohana/orm.git',
+    ),
+    array(
+        'path' => 'modules/unittest',
+        'url' => 'https://github.com/kohana/unittest.git',
+    ),
+    array(
+        'path' => 'modules/userguide',
+        'url' => 'https://github.com/kohana/userguide.git',
+    ),
+    array(
+        'path' => 'modules/bootstrap',
+        'url' => 'https://github.com/ich/kohana-bootstrap.git',
+    ),
+    array(
+        'path' => 'modules/client',
+        'url' => 'https://github.com/ich/kohana-client-controller.git',
+    ),
+    array(
+        'path' => 'modules/twig',
+        'url' => 'https://github.com/ich/kohana-twig.git',
+    ),
+);
+
+foreach ($submodule as $module) {
+    echo "git submodule add {$module['url']} {$module['path']}\n";
+    echo `git submodule add {$module['url']} {$module['path']}`;
+}
+
+echo "git submodule update --init --recursive\n";
+echo `git submodule update --init --recursive`;
